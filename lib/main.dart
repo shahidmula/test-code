9d9834f4bb2c59faa905871ed874af2ee27d03d8
shahidmula <121085699+shahidmula@users.noreply.github.com>
2022-12-24 17:07:12 +0500

    test-code added

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+void main(List<String> args) {
+  runApp(Camera());
+}class Camera extends StatelessWidget {
+  const Camera({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
